d5f695320ac43bf54314502d4e365376865daef7
wengjianhong <wengjianhong2099@163.com>
2026-07-01 17:44:00 +0800

    build(cmake): add Threads dependency to cpputils configuration
    Included the Threads dependency in the CMake configuration for cpputils to ensure proper linking and functionality. This enhances the package's compatibility and usability in multi-threaded environments.

--- cmake/cpputils-config.cmake
+++ cmake/cpputils-config.cmake
@@ -1,3 +1,7 @@
 # Entry point for find_package(cpputils CONFIG).
 # Defines imported target: cpputils::cpputils
+
+include(CMakeFindDependencyMacro)
+find_dependency(Threads)
+
 include("${CMAKE_CURRENT_LIST_DIR}/cpputilsTargets.cmake")
